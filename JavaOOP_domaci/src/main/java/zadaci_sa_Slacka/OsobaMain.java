2387763dfd1a20ab0e5e788e55a63736ba47e8fa
Nikola Ilic <nikola.ilic12358@gmail.com>
2022-05-24 12:33:30 +0200

    Zadaci sa Slacka

---
+++ JavaOOP_domaci/src/main/java/zadaci_sa_Slacka/OsobaMain.java
@@ -0,0 +1,26 @@
+package zadaci_sa_Slacka;
+
+public class OsobaMain {
+
+    public static void main(String[] args) {
+
+        Osoba osoba = new Osoba("Nikola Ilic");
+        Osoba majka = new Osoba("Slavica Ilic");
+        Osoba otac = new Osoba("Goran Ilic");
+        Osoba ocevOtac = new Osoba("Borivoje Ilic");
+        Osoba ocevaMajka = new Osoba("Desanka Ilic");
+        Osoba majcinOtac = new Osoba("Vladan Zivkovic");
+        Osoba majcinaMajka = new Osoba("Darinka Zivkovic");
+
+
+        osoba.setMajka(majka);
+        osoba.setOtac(otac);
+        osoba.getOtac().setOtac(ocevOtac);
+        osoba.getOtac().setMajka(ocevaMajka);
+        osoba.getMajka().setOtac(majcinOtac);
+        osoba.getMajka().setMajka(majcinaMajka);
+
+        osoba.print();
+    }
+}
+
